67fefb13243e449427c21e1dfc06ce27480a28f7
SwiatekFilip <fswiatek@student.agh.edu.pl>
2019-06-30 14:23:57 +0200

    poprawki

--- sampler.vhd
+++ sampler.vhd
@@ -4,13 +4,6 @@ use IEEE.std_logic_unsigned.all;
 use ieee.numeric_std.all;
 
 
-
-
-
-
-
-
-
 entity sampler is
 
 port    (
@@ -19,7 +12,7 @@ port    (
             CLK: in std_logic; -- global clock
             SCE: in std_logic;  -- clock enable from prescaler (for sampler only - read clock is not prescaled)
 				ADDRQ: in std_logic_vector (10 downto 0); -- sample address to show on output
-				Q: out std_logic_vector(7 downto 0) -- output
+				Q: out std_logic_vector(7 downto 0) -- output made to connect with data in from memory
          );
 end sampler;
 
@@ -29,22 +22,10 @@ constant SAMPLES_num : integer := 1280; -- number of samples
 
 signal X_first: std_logic_vector(7 downto 0);
 signal ADDR_x: std_logic_vector(10 downto 0);
-signal TRIGGERED: std_logic;
+signal TRIGGER: std_logic;
 
 begin
-trigger:process(CLK, RST)
-begin
-if rising_edge(CLK) then
-	if RST = '1' then
-		X_first <= X;
-		TRIGGERED <= 0;
-	else
-		if( X_first xor X /= "00000000") then
-			TRIGGERED <= '1';
-		end if;
-	end if;
-end if;
-end process;
+
 
 sampling:process(CLK, RST)
 begin
@@ -52,14 +33,17 @@ begin
 if rising_edge(CLK) then
 	if RST = '1' then
 		ADDR_x <= "00000000000";
+		X_first <= X;
+		TRIGGER <= '0';
 	else
-		if(CE = '1') then
-			if(TRIGGERED) then
+		if(SCE = '1') then
+			if(TRIGGER = '1' or ((X_first xor X) /= "00000000")) then
 				if(ADDR_x /=  SAMPLES_num - 1) then -- save input state to buffer
-					ADDR_x <= ADDR_x + 1;
-					-- tuaj bedzie wpisywanie danej do pamieci
+				TRIGGER <= '1';
+				ADDR_x <= ADDR_x + 1;
+				Q <= X;
 				end if;
-			end if; -- if(TRIGGERED)	
+			end if; -- if(TRIGGER)	
 		end if; --if(CE = '1')
 	end if; -- if RST = '1'
 end if; --if rising_edge(CLK)
